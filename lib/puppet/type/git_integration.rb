5954007085f5137e469b9f91d9c155919fc302c9
Brian Schonecker <brian.schonecker@nfiindustries.com>
2023-05-09 08:34:01 -0400

    foo

--- lib/puppet/type/git_integration.rb
+++ lib/puppet/type/git_integration.rb
@@ -43,8 +43,8 @@ module Puppet
       end
     end
 
-    newproperty(:notify_only_broken_pipelines) do
-    #newproperty(:notify_only_broken_pipelines, :boolean => true, :parent => Puppet::Parameter::Boolean) do
+    #newproperty(:notify_only_broken_pipelines) do
+    newproperty(:notify_only_broken_pipelines, :boolean => true, :parent => Puppet::Parameter::Boolean) do
       desc 'Send notifications for broken pipelines.'
       defaultto 'this is the default value.'
     end
