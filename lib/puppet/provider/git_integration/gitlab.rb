231bf539fa5f24a89c63a3a3e0eec16abea39ff6
Brian Schonecker <brian.schonecker@nfiindustries.com>
2023-05-09 11:09:38 -0400

    foo

--- lib/puppet/provider/git_integration/gitlab.rb
+++ lib/puppet/provider/git_integration/gitlab.rb
@@ -149,9 +149,7 @@ Puppet::Type.type(:git_integration).provide(:gitlab) do
     begin
       opts = { 'webhook' => resource[:webhook].strip }
 
-      if branches_to_be_notified?
-        opts['branches_to_be_notified'] = resource[:branches_to_be_notified]
-      end
+      opts['branches_to_be_notified'] = resource[:branches_to_be_notified]
       opts['confidential_issues_events'] = resource[:confidential_issues_events]
       opts['confidential_note_events'] = resource[:confidential_note_events]
       opts['issues_events'] = resource[:issues_events]
