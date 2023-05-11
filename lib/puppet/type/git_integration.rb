c4dd43591a7f717aba7f3485785b7dabb3e7085c
Brian Schonecker <brian.schonecker@nfiindustries.com>
2023-05-11 15:43:52 -0400

    foo

--- lib/puppet/type/git_integration.rb
+++ lib/puppet/type/git_integration.rb
@@ -54,11 +54,8 @@ module Puppet
 
     newproperty(:branches_to_be_notified) do
       desc 'Branches to send notifications for. Valid options are all, default, protected, and default_and_protected. The default value is “default”.'
-      newvalues(all, default, protected, default_and_protected)
-      def insync?(is)
-        is.to_s == should.to_s
-      end
-      defaultto :default
+      newvalues('all', 'default', 'protected', 'default_and_protected')
+      defaultto 'default'
     end
 
     newproperty(:push_events, :boolean => true) do
