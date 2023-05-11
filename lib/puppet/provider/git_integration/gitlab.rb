428491786663d15ede37af265413369e01f1a6b1
Brian Schonecker <brian.schonecker@nfiindustries.com>
2023-05-11 09:33:02 -0400

    foo

--- lib/puppet/provider/git_integration/gitlab.rb
+++ lib/puppet/provider/git_integration/gitlab.rb
@@ -310,7 +310,7 @@ Puppet::Type.type(:git_integration).provide(:gitlab) do
 
     begin
       opts = { 'webhook' => resource[:webhook].strip }
-      opts["#{__method__}"] = "#{value}"
+      opts["#{caller[0]}"] = "#{value}"
       Puppet.debug("YYYY: opts: #{opts}.")
 
       response = api_call('PUT', url, opts)
