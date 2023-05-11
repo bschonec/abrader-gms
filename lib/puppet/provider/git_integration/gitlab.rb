dfa07951183f1c474b001eff6c2c405bc577cfc0
Brian Schonecker <brian.schonecker@nfiindustries.com>
2023-05-11 09:16:55 -0400

    foo

--- lib/puppet/provider/git_integration/gitlab.rb
+++ lib/puppet/provider/git_integration/gitlab.rb
@@ -295,11 +295,11 @@ Puppet::Type.type(:git_integration).provide(:gitlab) do
     response = api_call('GET', url)
     integration_json = JSON.parse(response.body)
     Puppet.debug("wiki_page_events: #{integration_json['wiki_page_events']}.")
-    integration_json['wiki_page_events']?
+    integration_json['wiki_page_events']
   end
 
   def wiki_page_events=(value)
-    do_the_needfull(__method__, value)
+    do_the_needfull(__method__, :value)
   end
 
   def do_the_needfull(param, value)
